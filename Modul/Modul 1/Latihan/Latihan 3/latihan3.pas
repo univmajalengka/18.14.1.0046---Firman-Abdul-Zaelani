f6dc7c9aa8ebe032d157073738b47e2fff836cea
firmannnnn <50068787+firmannnnn@users.noreply.github.com>
2021-03-22 21:45:36 +0700

    Add files via upload

---
+++ Modul/Modul 1/Latihan/Latihan 3/latihan3.pas
@@ -0,0 +1,16 @@
+{Nama           : Firman Abdul ZAelani }
+{NPM            : 18.14.1.0046         }
+{Nama Program   : hello3.pas           }
+
+program hello3;
+uses crt;
+
+var
+nama : string;
+
+begin
+CLRSCR;
+writeln ('Contoh membaca dan menulis inputan');
+write ('Masukkan nama : ');readln(nama);
+
+end.

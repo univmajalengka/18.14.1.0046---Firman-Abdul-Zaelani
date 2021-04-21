85050d55008825d909347136eff5fa9cac641b3f
firmannnnn <50068787+firmannnnn@users.noreply.github.com>
2021-04-21 23:41:21 +0700

    Add files via upload

---
+++ Modul/Modul 1/Latihan/Latihan2.pas
@@ -0,0 +1,21 @@
+{Nama           : Firman Abdul Zaelani  }
+{NPM            : 18.14.1.0046          }
+{Nama Program   : hello2.pas            }
+
+program hello2;
+uses crt;
+
+var
+i : integer;
+hello : string;
+
+begin
+CLRSCR;
+hello := 'Hello, dunia';
+i:= 1234;
+writeln(hello);
+writeln('Nilai i = ',i);
+
+readln;
+
+end.
